f8f29f3c2d33947846173c7d5831ea32dea4565f
irfan-ahmed24 <irfanahmed99989@gmail.com>
2025-07-28 00:12:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// var dropdownMenu = document.getElementById("dropdown");
+// var dropdownButton = document.getElementById("dropdown-meno");
+// dropdownButton.addEventListener("click", function () {
+//   document.getElementById("dropdown").classList.toggle("hidden");
+//   document.getElementById("dropdown").classList.toggle("animate-dropdown");
+// });
